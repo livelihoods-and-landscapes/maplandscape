0bcad7c603c484f64392cb118f910aa296a3427e
jd <jmad1v07@gmail.com>
2022-09-04 10:08:55 +0800

    update docs

--- README.md
+++ README.md
@@ -2,113 +2,51 @@
 
 ## Overview
 
-A package that provides functions for building [Shiny](https://shiny.rstudio.com) dashboard applications to explore and visualise spatial layers in GeoPackages. In particular, it focuses on providing browser-based tools for analysing data stored in GeoPackages in QFieldCloud. 
+A package that provides functions for building [Shiny](https://shiny.rstudio.com) dashboard applications to explore and visualise layers (tables) in GeoPackages. It focuses on providing tools for quick analysis of data collected in-the-field using the [QField](https://qfield.org) mobile GIS application and stored in GeoPackages in QFieldCloud. 
 
 A pre-built [Shiny](https://shiny.rstudio.com) application is provided with this package which uses these functions and provides tools for:
 
-* Syncing data stored in multiple GeoPackages.
 * Exploring GeoPackage layers in interactive tables, web maps, and charts.
 * Customising and styling charts and web maps. 
 * Combining layers using spatial and non-spatial joins.
 * Generating summary tables through group-by and summarise operations. 
 * Generating new layers and add new columns to existing layers. 
-* Authenticated access to GeoPackages stored in QFieldCloud and Google Cloud Storage.
+* Authenticated access to GeoPackages stored in QFieldCloud.
 
-This package was developed as part of the [Livelihoods and Landscapes](https://livelihoods-and-landscapes.com) project which is developing tools and approaches to map diverse agricultural landscapes. The initial motivation for developing the package and Shiny application was to provide analysts with tools to analyse and visualise geospatial data collected in-the-field using the [QField](https://qfield.org) mobile GIS application. 
+This package was developed as part of the [Livelihoods and Landscapes](https://livelihoods-and-landscapes.com) project which is developing tools and approaches to map diverse agricultural landscapes.  
 
 ## Setup
 
 ### Local R Package
 
-Install as an R package:
-
-```
-install.packages("devtools")
-devtools::install_github("livelihoods-and-landscapes/maplandscape")
-```
-
-Build and customise your own Shiny application using functions provided by maplandscape or launch a pre-built application:
-
-```
-dir <- system.file("app", package = "maplandscape")
-setwd(dir)
-library(maplandscape)
-shiny::shinyAppDir(".")
-```
-
-To build the package use tools provided by the [devtools](https://github.com/r-lib/devtools) package:
-
-```
-# get package dependencies
-renv::restore()
-
-# check package
-devtools::check()
-
-# update NAMESPACE file for exports
-devtools::document() 
-
-# build docs
-pkgdown::build_site()
-```
-
-The will launch a pre-built Shiny application to explore data in GeoPackages from the `inst/app` sub-directory of the package. 
-
-### shinyapps.io
-
-RStudio offers a free-tier hosting service for Shiny apps. Sign up at [shinyapps.io](https://www.shinyapps.io). 
-
-Follow the above commands to launch maplandscape (or your custom Shiny application). In the top-right of the application window you will see a *Publish* button, click this button and follow the prompts to deploy the application to [shinyapps.io](https://www.shinyapps.io).
-
-<img src="man/figures/shiny-publish.png" width="40%" style="display: block; margin: auto;"/>
-
-RStudio provide a detailed tutorial for deploying apps to shinyapps.io [here](https://shiny.rstudio.com/articles/shinyapps.html).
-
-
-### Docker and host in the cloud
-
-This requires installing [git](https://git-scm.com/downloads) and [docker](https://www.docker.com). Clone the maplandscape GitHub repo:
+Clone the maplandscape repo:
 
 ```
 git clone https://github.com/livelihoods-and-landscapes/maplandscape.git
+```
 
+Move into the maplandscape directory:
+
+```
 cd maplandscape
 ```
 
-#### Docker and Shiny Server
-
-There is a sub-directory named `docker-shiny-server`. This contains a `Dockerfile` that lists instructions that are used to build a docker image.
+maplandscape requires an instance of PostGIS. Deploy a local PostGIS instance using docker:
 
 ```
-cd /inst/docker-shiny-server
-
-docker build -t maplandscape -f ShinyServer.Dockerfile .
+cd inst/postgis
+docker compose up -d
 ```
 
-The image is based on the `rocker/shiny:latest` image which includes [Shiny Server](https://www.rstudio.com/products/shiny/shiny-server/?_ga=2.240850435.1437924050.1628840494-908324396.1627896044) to host the maplandscape Shiny application. Shiny Server serves apps out of the `srv/shiny-server/` directory; building the docker image will install all the R packages required to run maplandscape, install the maplandscape package from github, and copy an `app.R` script into `srv/shiny-server/app` which contains the commands to launch maplandscape. 
+Make sure the PostGIS configuration in `inst/app/config` matches what is specified in the `docker-compose.yml` file. 
 
-A customised `shiny-customised.config` file is used to set the `app_dir` to  the app directory where Shiny Server will launch and serve the app from. 
-
-Launch the docker container:
+Move the root of the maplandscape repo. Launch the app:
 
 ```
-docker run -p 3838:3838 maplandscape
+cd ../..
+shiny::runApp("inst/app")
 ```
 
-You can find more information about [Shiny Server](https://www.rstudio.com/products/shiny/shiny-server/?_ga=2.240850435.1437924050.1628840494-908324396.1627896044) [here](https://shiny.rstudio.com/articles/shiny-server.html), at its [GitHub repo](https://github.com/rstudio/shiny-server), and following this [tutorial](https://deanattali.com/2015/05/09/setup-rstudio-shiny-server-digital-ocean/). 
+### Docker and deploy to the cloud
 
-Please see the vignette Deploy: Google Cloud for a tutorial demonstrating how to deploy a containerised Shiny application on Google Cloud Run. 
-
-#### Docker 
-
-To deploy as a containerised Shiny application without Shiny Server (e.g. if deploying using Shiny Proxy) use the `Dockerfile` in the `/inst/` directory. This `Dockerfile` is based of a generic `maplandscape-base` image which is pre-built on top of Ubuntu 20.04 LTS and the rocker r-ver 4.1.2 image. It contains the system libraries and main R package dependencies used to build maplandscape. 
-
-```
-docker build -t maplandscape .
-```
-And to run:
-
-```
-docker run -p 3838:3838 maplandscape
-```
 
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -175,11 +175,11 @@ navbarPage(
         id = "table-options",
         class = "rounded border border-primary m-3 pt-3 pl-3 pr-3 overflow-scroll",
         draggable = TRUE,
-        top = 65,
-        right = 20,
+        top = 195,
+        left = 15,
         checkboxInput(
           "table_options",
-          htmltools::HTML("Table options &#8595;"),
+          htmltools::HTML("<b>Table options &#8595;</b>"),
           FALSE
         ),
         conditionalPanel(
@@ -204,310 +204,158 @@ navbarPage(
               "Join layers"
             ),
           ),
-            conditionalPanel(
-              class = "p-2",
-              condition = "input.table_analysis == 'Filter'",
-              h4("Filter rows"),
-              selectInput(
-                "table_filter",
-                label = "Select layer to filter:",
-                choices = NULL
-              ),
-              div(
-                class = "d-flex justify-content-center",
-                actionButton(
-                  "filter",
-                  "Filter rows options",
-                  class = "btn btn-block m-2"
-                ),
-              )
+          conditionalPanel(
+            class = "p-2",
+            condition = "input.table_analysis == 'Filter'",
+            h4("Filter rows"),
+            selectInput(
+              "table_filter",
+              label = "Select layer to filter:",
+              choices = NULL
             ),
-            conditionalPanel(
-              class = "p-2",
-              condition = "input.table_analysis == 'Add column'",
-              h4("Add new column"),
-              selectInput(
-                "table_mutate",
-                label = "Select layer to add new column:",
-                choices = NULL
+            div(
+              class = "d-flex justify-content-center",
+              actionButton(
+                "filter",
+                "Filter rows options",
+                class = "btn btn-block m-2"
               ),
-              div(
-                class = "d-flex justify-content-center",
-                actionButton(
-                  "add_column",
-                  "Add column options",
-                  class = "btn btn-block m-2"
-                ),
-              )
+            )
+          ),
+          conditionalPanel(
+            class = "p-2",
+            condition = "input.table_analysis == 'Add column'",
+            h4("Add new column"),
+            selectInput(
+              "table_mutate",
+              label = "Select layer to add new column:",
+              choices = NULL
             ),
-            conditionalPanel(
-              class = "p-2",
-              condition = "input.table_analysis == 'Summary tables'",
-              h4("Summary tables"),
-              mod_multiple_input_UI(
-                id = "grouping_var",
-                label = "Grouping variable(s):"
+            div(
+              class = "d-flex justify-content-center",
+              actionButton(
+                "add_column",
+                "Add column options",
+                class = "btn btn-block m-2"
               ),
-              mod_multiple_input_UI(
-                id = "summarising_var",
-                label = "Summarising variable(s):"
-              )
+            )
+          ),
+          conditionalPanel(
+            class = "p-2",
+            condition = "input.table_analysis == 'Summary tables'",
+            h4("Summary tables"),
+            mod_multiple_input_UI(
+              id = "grouping_var",
+              label = "Grouping variable(s):"
             ),
-            conditionalPanel(
-              class = "p-2",
-              condition = "input.table_analysis == 'Join layers'",
-              h4("Join layers"),
-              selectInput(
-                "table_left",
-                label = "Select left layer in join:",
-                choices = NULL
+            mod_multiple_input_UI(
+              id = "summarising_var",
+              label = "Summarising variable(s):"
+            )
+          ),
+          conditionalPanel(
+            class = "p-2",
+            condition = "input.table_analysis == 'Join layers'",
+            h4("Join layers"),
+            selectInput(
+              "table_left",
+              label = "Select left layer in join:",
+              choices = NULL
+            ),
+            selectInput(
+              "table_right",
+              label =
+                "Select right layer in join:",
+              choices = NULL
+            ),
+            mod_multiple_input_UI(
+              id = "joining_p_key_left",
+              label = "Select primary key(s) - left layer:"
+            ),
+            mod_multiple_input_UI(
+              id = "joining_f_key_right",
+              label = "Select foreign key(s) - right layer:"
+            ),
+            radioButtons(
+              "key_join_type",
+              "Join Type:",
+              c(
+                "column - inner" = "col_inner",
+                "column - left" = "col_left"
               ),
-              selectInput(
-                "table_right",
-                label =
-                  "Select right layer in join:",
-                choices = NULL
+              selected = NULL
+            ),
+            textInput(
+              "join_tbl_name",
+              "Layer name:",
+              value = "",
+              placeholder = "enter layer name for output"
+            ),
+            div(
+              class = "d-flex justify-content-center",
+              actionButton(
+                "table_join_button",
+                "Apply join",
+                class = "btn btn-block m-2"
               ),
-              mod_multiple_input_UI(
-                id = "joining_p_key_left",
-                label = "Select primary key(s) - left layer:"
-              ),
-              mod_multiple_input_UI(
-                id = "joining_f_key_right",
-                label = "Select foreign key(s) - right layer:"
-              ),
-              radioButtons(
-                "key_join_type",
-                "Join Type:",
-                c(
-                  "column - inner" = "col_inner",
-                  "column - left" = "col_left"
-                ),
-                selected = NULL
-              ),
-              textInput(
-                "join_tbl_name",
-                "Layer name:",
-                value = "",
-                placeholder = "enter layer name for output"
-              ),
-              div(
-                class = "d-flex justify-content-center",
-                actionButton(
-                  "table_join_button",
-                  "Apply join",
-                  class = "btn btn-block m-2"
-                ),
-              ),
-              hr(),
-              h4("Join spatial layers"),
-              selectInput(
-                "spatial_table_left",
-                label = "Select left layer in join:",
-                choices = NULL
-              ),
-              selectInput(
-                "spatial_table_right",
-                label = "Select right layer in join:",
-                choices = NULL
-              ),
-              textInput(
-                "spjoin_tbl_name",
-                "Layer name:",
-                value = "",
-                placeholder = "enter layer name for output"
-              ),
-              div(
-                class = "d-flex justify-content-center",
-                actionButton(
-                  "spatial_join_button",
-                  "Apply spatial join",
-                  class = "btn btn-block m-2"
-                ),
-              ),
-            ), # table analysis
-          ), # table options - conditional panel
-        ), # table options - absolute panel
-        tabsetPanel(
-          id = "data_tables",
-          type = "tabs",
-          tabPanel(
-            "Data: Raw",
-            downloadButton(
-              "download_data_raw",
-              "Download Data",
-              class = "btn m-2"
             ),
             hr(),
-            mod_render_dt_UI(id = "data_raw")
-          ),
-          tabPanel(
-            "Data: Summary",
-            downloadButton(
-              "download_data_summarised",
-              "Download Summarised Data",
-              class = "btn m-2"
+            h4("Join spatial layers"),
+            selectInput(
+              "spatial_table_left",
+              label = "Select left layer in join:",
+              choices = NULL
             ),
-            hr(),
-            mod_render_dt_UI(id = "data_summary")
+            selectInput(
+              "spatial_table_right",
+              label = "Select right layer in join:",
+              choices = NULL
+            ),
+            textInput(
+              "spjoin_tbl_name",
+              "Layer name:",
+              value = "",
+              placeholder = "enter layer name for output"
+            ),
+            div(
+              class = "d-flex justify-content-center",
+              actionButton(
+                "spatial_join_button",
+                "Apply spatial join",
+                class = "btn btn-block m-2"
+              ),
+            ),
+          ), # table analysis
+        ), # table options - conditional panel
+      ), # table options - absolute panel
+      tabsetPanel(
+        id = "data_tables",
+        type = "tabs",
+        tabPanel(
+          "Data: Raw",
+          downloadButton(
+            "download_data_raw",
+            "Download Data",
+            class = "btn m-2"
           ),
+          hr(),
+          mod_render_dt_UI(id = "data_raw")
         ),
-      ), # fluid page
-    ), # tab panel
+        tabPanel(
+          "Data: Summary",
+          downloadButton(
+            "download_data_summarised",
+            "Download Summarised Data",
+            class = "btn m-2"
+          ),
+          hr(),
+          mod_render_dt_UI(id = "data_summary")
+        ),
+      ),
+    ), # fluid page
+  ), # tab panel
 
 
-    # sidebarLayout(
-    #   sidebarPanel(
-    #     class = "sidePanelStyle",
-    #
-    #     # Select active layer
-    #     h4("Active layer"),
-    #     selectInput(
-    #       "active_layer",
-    #       "Select active layer:",
-    #       choices = NULL
-    #     ),
-    #     hr(),
-    #     h4("Filter rows"),
-    #     selectInput(
-    #       "table_filter",
-    #       label = "Select layer to filter:",
-    #       choices = NULL
-    #     ),
-    #     div(
-    #       class = "d-flex justify-content-center",
-    #       actionButton(
-    #         "filter",
-    #         "Filter rows options",
-    #         class = "btn btn-block m-2"
-    #       ),
-    #     ),
-    #     hr(),
-    #     h4("Add new column"),
-    #     selectInput(
-    #       "table_mutate",
-    #       label = "Select layer to add new column:",
-    #       choices = NULL
-    #     ),
-    #     div(
-    #       class = "d-flex justify-content-center",
-    #       actionButton(
-    #         "add_column",
-    #         "Add column options",
-    #         class = "btn btn-block m-2"
-    #       ),
-    #     ),
-    #     hr(),
-    #     # Summary tables
-    #     h4("Summary tables"),
-    #     mod_multiple_input_UI(
-    #       id = "grouping_var",
-    #       label = "Grouping variable(s):"
-    #     ),
-    #     mod_multiple_input_UI(
-    #       id = "summarising_var",
-    #       label = "Summarising variable(s):"
-    #     ),
-    #     hr(),
-    #     h4("Combine layers"),
-    #     selectInput(
-    #       "table_left",
-    #       label = "Select left layer in join:",
-    #       choices = NULL
-    #     ),
-    #     selectInput(
-    #       "table_right",
-    #       label =
-    #         "Select right layer in join:",
-    #       choices = NULL
-    #     ),
-    #     mod_multiple_input_UI(
-    #       id = "joining_p_key_left",
-    #       label = "Select primary key(s) - left layer:"
-    #     ),
-    #     mod_multiple_input_UI(
-    #       id = "joining_f_key_right",
-    #       label = "Select foreign key(s) - right layer:"
-    #     ),
-    #     radioButtons(
-    #       "key_join_type",
-    #       "Join Type:",
-    #       c(
-    #         "column - inner" = "col_inner",
-    #         "column - left" = "col_left"
-    #       ),
-    #       selected = NULL
-    #     ),
-    #     textInput(
-    #       "join_tbl_name",
-    #       "Layer name:",
-    #       value = "",
-    #       placeholder = "enter layer name for output"
-    #     ),
-    #     div(
-    #       class = "d-flex justify-content-center",
-    #       actionButton(
-    #         "table_join_button",
-    #         "Apply join",
-    #         class = "btn btn-block m-2"
-    #       ),
-    #     ),
-    #     hr(),
-    #     h4("Combine spatial layers"),
-    #     selectInput(
-    #       "spatial_table_left",
-    #       label = "Select left layer in join:",
-    #       choices = NULL
-    #     ),
-    #     selectInput(
-    #       "spatial_table_right",
-    #       label = "Select right layer in join:",
-    #       choices = NULL
-    #     ),
-    #     textInput(
-    #       "spjoin_tbl_name",
-    #       "Layer name:",
-    #       value = "",
-    #       placeholder = "enter layer name for output"
-    #     ),
-    #     div(
-    #       class = "d-flex justify-content-center",
-    #       actionButton(
-    #         "spatial_join_button",
-    #         "Apply spatial join",
-    #         class = "btn btn-block m-2"
-    #       ),
-    #     ),
-    #  ),
-
-      ## show data tables
-      # mainPanel(
-      #   class = "mainPanelStyle",
-      #   tabsetPanel(
-      #     id = "data_tables",
-      #     type = "tabs",
-      #     tabPanel(
-      #       "Data: Raw",
-      #       downloadButton(
-      #         "download_data_raw",
-      #         "Download Data",
-      #         class = "btn m-2"
-      #       ),
-      #       hr(),
-      #       mod_render_dt_UI(id = "data_raw")
-      #     ),
-      #     tabPanel(
-      #       "Data: Summary",
-      #       downloadButton(
-      #         "download_data_summarised",
-      #         "Download Summarised Data",
-      #         class = "btn m-2"
-      #       ),
-      #       hr(),
-      #       mod_render_dt_UI(id = "data_summary")
-      #     )
-      #   )
-      # ),
 
   # Map Tab -----------------------------------------------------------------
 
@@ -522,7 +370,7 @@ navbarPage(
         left = 50,
         checkboxInput(
           "map_options",
-          htmltools::HTML("Map options &#8595;"),
+          htmltools::HTML("<b>Map options &#8595;</b>"),
           FALSE
         ),
         conditionalPanel(
